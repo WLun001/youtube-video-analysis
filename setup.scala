7c4c5766d32c864a49dbb0f0c39ed4177173c870
Wei Lun <weilun_95@hotmail.com>
2018-04-07 23:44:15 +0800

    added deletion of duplicate data

--- setup.scala
+++ setup.scala
@@ -17,3 +17,13 @@ val videosTuple = videosTabRemoved.map(x => x match{
  case withDesc if(withDesc.size==16) => (x(0),x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),x(10),x(11),x(12),x(13),x(14),x(15)); 
  case withoutDesc if(withoutDesc.size==15) => (x(0),x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),x(10),x(11),x(12),x(13),x(14),"no description"); 
  case error => ("dropThisTuple","","","","","","","","","","","","","","","") })
+
+//remove duplicate data
+val keyValue = videosTuple.map(x => 
+	(x._1, (x._2, x._3, x._4, x._5, x._6, x._7, x._8, x._9, x._10, x._11, x._12, x._13, x._14, x._15, x._16)))
+
+val reduceRDD= keyValue.map(tup =>
+    (tup._1, tup)).reduceByKey { case (a, b) => a }.map(_._2)
+
+val formattedrdd = reduceRDD.map(x => (x._1, x._2._1, x._2._2, x._2._3, x._2._4,
+ x._2._5, x._2._6, x._2._7, x._2._8, x._2._9, x._2._10, x._2._11, x._2._12, x._2._13, x._2._14, x._2._15))
