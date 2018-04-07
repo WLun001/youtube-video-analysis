2d6eaf95fb6c094054e569f0b80cf47cdc189643
Wei Lun <weilun_95@hotmail.com>
2018-04-07 23:52:23 +0800

    added read and save to Parquet

---
+++ readFromParquet.scala
@@ -0,0 +1,7 @@
+//assume there is data in parquet
+val parquetRDD = sqlContext.sql("SELECT * FROM parquet.`/user/cloudera/labs/videos.parquet`")
+parquetrdd.count
+
+//cast to relevant data type
+val castedRDD = parquetrdd.map(x => (x(0).asInstanceOf[String], x(1).asInstanceOf[String], x(2).asInstanceOf[String], x(3).asInstanceOf[String], x(4).asInstanceOf[String], x(5).asInstanceOf[String], x(6).asInstanceOf[String], x(7).asInstanceOf[String], x(8).asInstanceOf[String], x(9).asInstanceOf[String], x(10).asInstanceOf[String], x(11).asInstanceOf[String], x(12).asInstanceOf[String], x(13).asInstanceOf[String], x(14).asInstanceOf[String], x(15).asInstanceOf[String]))
+//continue with Spark SQL
